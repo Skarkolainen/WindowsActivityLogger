74768ce33a12e142201bb2af2134963632de3146
Simen Skarkerud <simen.skarkerud@bouvet.no>
2025-01-28 14:45:56 +0100

    AddWindowsService

--- src/WindowsActivityLogger/Program.cs
+++ src/WindowsActivityLogger/Program.cs
@@ -1,6 +1,8 @@
 using WindowsActivityLogger;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddWindowsService();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
